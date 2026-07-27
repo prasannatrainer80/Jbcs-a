fe2cee58a973e39361a4c7ecef0fc70aac903a2d
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-27 11:53:46 +0530

    code

---
+++ July-27/Prog10.py
@@ -0,0 +1,9 @@
+sno=int(input("Enter Your No"))
+if sno==1:
+    print("Hi I am Vishnu...")
+elif sno==2:
+    print("Hi I am Gopi")
+elif sno==3:
+    print("Hi I am Kshithij")
+elif sno==4:
+    print("Hi I am ANitha")
---
+++ July-27/Prog11.py
@@ -0,0 +1,10 @@
+# Program to display max of 3 numbers
+a = int(input("Enter First Number  "))
+b = int(input("Enter Second Number  "))
+c = int(input("Enter Third Number  "))
+m = a
+if m < b:
+    m = b
+if m < c:
+    m = c
+print("Max Value is  ", m)
