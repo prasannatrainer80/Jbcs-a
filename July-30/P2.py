db2dcd32bbae3febcd0e647f08172054ae89ba4b
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-30 13:47:23 +0530

    code

---
+++ July-30/P2.py
@@ -0,0 +1,10 @@
+def show(firstName,lastName,city,state):
+    print("First-Name  ", firstName)
+    print("Last-Name  ", lastName)
+    print("City  ", city)
+    print("State  ", state)
+
+#show("Delhi","UP","Abhinay",
+ #    "Sreyan")
+show(city="Delhi",state="Up",firstName="Abhinay",
+     lastName="Sreyan")
