479ad82c8eab9df75a516d0be9a80f770c6a7292
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-30 13:31:51 +0530

    code

---
+++ July-30/P1.py
@@ -0,0 +1,10 @@
+def show(*names):
+    for i in names:
+        print(i, end=" ")
+    print()
+
+show()
+show("Anvith")
+show("Ganesh","Anvith")
+show("Pradeep","Ganesh","Anvith")
+show("Saba","Anitha","Srinitya","Deepika")
